01a928e7690c03e80766bb4603227a414e971825
atulbv <vernekaratul@gmail.com>
2015-04-27 19:18:01 +0530

    MusicStore
    This is MVC Application

---
+++ MusicStore/Models/MusicStoreContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MusicStore.Models
+{
+    public class MusicStoreContext : DbContext
+    {
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<Album> Albums { get; set; }
+        public DbSet<Artist> Artists { get; set; }
+    
+    }
+}
